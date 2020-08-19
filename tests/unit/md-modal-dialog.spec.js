1eef5735ba38ebf94fe2788a6a08f7a851ef52d8
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2020-08-19 14:35:49 +0200

    Fix build output

--- tests/unit/md-modal-dialog.spec.js
+++ tests/unit/md-modal-dialog.spec.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import { mount } from '@vue/test-utils'
 
-import MdModalDialog from '@/../dist/md-modal-dialog.esm'
-import '@/../dist/md-modal-dialog.css'
+import MdModalDialog from '@/../dist/components.esm'
+import '@/../dist/components.css'
 import MdModalDialogMock from './md-modal-dialog.mock.vue'
 
 
