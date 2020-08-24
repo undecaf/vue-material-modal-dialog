422f30ed36d7fd396d3443f4f688fb787d0fbbba
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2020-08-24 09:26:59 +0200

    Support object decomposition in tests

--- rollup.config.js
+++ rollup.config.js
@@ -32,6 +32,7 @@ const baseConfig = {
         postVue: [
             buble({
                 transforms: {
+                    objectAssign: 'Object.assign',
                     dangerousForOf: true,
                 },
             }),
